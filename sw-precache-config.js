1ad03b7b63c053718c974d0f6c61edbbb06d0d8e
edoh <edoh.dev@gmail.com>
2018-05-19 22:05:33 +0200

    initial commit service worker

---
+++ sw-precache-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stripPrefix: 'build/',
+  staticFileGlobs: [
+    'build/*.html',
+    'build/manifest.json',
+    'build/static/**/!(*map*)'
+  ],
+  dontCacheBustUrlsMatching: /\.\w{8}\./,
+  swFilePath: 'build/service-worker.js'/*,
+  runtimeCaching: [{
+    // urlPattern: /^https:\/\/example\.com\/api/,
+    urlPattern: /^https:\/\/maps\.googleapis\/com/,
+    handler: 'networkFirst'
+  }]*/
+};
